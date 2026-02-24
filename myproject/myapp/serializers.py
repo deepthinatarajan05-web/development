48046829b0237417ea3ca9a45468eebcdeb2d5bc
deepthinatarajan05-web <deepthinatarajan05@gmail.com>
2026-02-24 12:36:05 +0530

    deleted

--- myproject/myapp/serializers.py
+++
@@ -1,6 +0,0 @@
-from .models import student
-from  rest_framework import serializers
-class StudentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=student
-        fields='__all__'
--- myproject/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from .views import studentlist
-
-urlpatterns = [
-    path('ad/',studentlist),
-    path('ad/<int:id>/',studentlist),
-]
